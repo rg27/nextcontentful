662d1e384c22388c37f6e85beae8c48f929340b5
rg27 <gualvezreymar@gmail.com>
2021-04-11 22:28:06 +0800

    Contentful Client

---
+++ components/Layout.js
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+
+const Layout = ({children}) => {
+    return (  
+        <div className="layout">
+            <header>
+                <Link href="/">
+                    <a>
+                        <h1>
+                            <span>ASPO</span>
+                            <span>Food House</span>
+                        </h1>
+                        <h2>Spread The Joy</h2>
+                    </a>
+                </Link>
+            </header>
+
+            <div className="page-content">
+                {children}
+            </div>
+
+            <footer>
+                <p>Copyright 2021</p>
+            </footer>
+
+           
+        </div>
+    );
+}
+ 
+export default Layout;
